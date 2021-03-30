c6f660b4c5cfc3b4a66515faf189dee11f9ca0a0
Flotyx <ftyx728520@gmail.com>
2021-03-30 20:07:32 +0800

    final ans commit

--- quiz1.js
+++ quiz1.js
@@ -1,10 +1,3 @@
-function Function( ){
-var btn = document.createElement("BUTTON");
-btn.innerHTML = "Change the document";
-document.body.appendChild(btn);
-btn.addEventListener("click", myFunction)
-}
-
 function  myFunction( ){
   document.getElementById("try").innerHTML="CSIE@CGU";
   document.getElementById("demo").innerHTML="怎麽那麽棒!!.";
